75bc40f89685ef7757267848548450c06254770f
sferey-pro <sferey.pro@gmail.com>
2025-09-11 09:48:42 +0200

    feat(infrastructure): Ajout des containers php-worker pour messenger

--- config/packages/messenger.php
+++ config/packages/messenger.php
@@ -32,26 +32,13 @@ return static function (FrameworkConfig $framework): void {
 
     $messenger->failureTransport('failed_default');
 
-    $messenger->transport('async_priority_high')
-        ->dsn(env('MESSENGER_TRANSPORT_DSN'))
-        ->failureTransport('failed_high_priority');
-
-    $messenger->transport('async_events')
-        ->dsn(env('MESSENGER_TRANSPORT_DSN'))
-        ->retryStrategy()
-            ->maxRetries(0)
-    ;
-
-    $messenger->transport('async_priority_low')
+    $messenger->transport('async')
         ->dsn(env('MESSENGER_TRANSPORT_DSN'));
 
     $messenger->transport('failed_default')
         ->dsn('doctrine://default?queue_name=failed_default');
 
-    $messenger->transport('failed_high_priority')
-        ->dsn('doctrine://default?queue_name=failed_high_priority');
-
     $messenger->routing(Query::class)->senders(['sync']);
     $messenger->routing(Command::class)->senders(['sync']);
-    $messenger->routing(DomainEvent::class)->senders(['async_events']);
+    $messenger->routing(DomainEvent::class)->senders(['async']);
 };
--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -30,7 +30,7 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	# Or about an error in project initialization
 	php bin/console -V
 
-	if grep -q ^DATABASE_URL= .env; then
+	if grep -q ^DATABASE_URL= .env && [ "${RUN_MIGRATIONS:-true}" = "true" ]; then
 		echo 'Waiting for database to be ready...'
 		ATTEMPTS_LEFT_TO_REACH_DATABASE=60
 		until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console dbal:run-sql -q "SELECT 1" 2>&1); do
